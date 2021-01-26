ecc6b4a96e90c46a03239f7b78c5159c797953a5
Ivan <21068310+ihlpo@users.noreply.github.com>
2021-01-26 12:59:55 +1100

    Started js fucntions

---
+++ script.js
@@ -0,0 +1,7 @@
+function compute_grade(){
+    console.log("hi")
+};
+
+let compute_button = document.getElementById("compute");
+
+document.getElementById("compute").addEventListener("click", compute_grade);
